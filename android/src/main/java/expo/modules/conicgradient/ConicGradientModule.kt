da24d5eed9650de6170516bf64c4536c0309ff28
Jakub Grzywacz <kontakt@jakubgrzywacz.pl>
2025-01-16 22:45:23 +0100

    feat: implement android

--- android/src/main/java/expo/modules/conicgradient/ConicGradientModule.kt
+++ android/src/main/java/expo/modules/conicgradient/ConicGradientModule.kt
@@ -5,46 +5,25 @@ import expo.modules.kotlin.modules.ModuleDefinition
 import java.net.URL
 
 class ConicGradientModule : Module() {
-  // Each module class must implement the definition function. The definition consists of components
-  // that describes the module's functionality and behavior.
-  // See https://docs.expo.dev/modules/module-api for more details about available components.
   override fun definition() = ModuleDefinition {
-    // Sets the name of the module that JavaScript code will use to refer to the module. Takes a string as an argument.
-    // Can be inferred from module's class name, but it's recommended to set it explicitly for clarity.
-    // The module will be accessible from `requireNativeModule('ConicGradient')` in JavaScript.
     Name("ConicGradient")
 
-    // Sets constant properties on the module. Can take a dictionary or a closure that returns a dictionary.
-    Constants(
-      "PI" to Math.PI
-    )
-
-    // Defines event names that the module can send to JavaScript.
-    Events("onChange")
-
-    // Defines a JavaScript synchronous function that runs the native code on the JavaScript thread.
-    Function("hello") {
-      "Hello world! 👋"
-    }
-
-    // Defines a JavaScript function that always returns a Promise and whose native code
-    // is by default dispatched on the different thread than the JavaScript runtime runs on.
-    AsyncFunction("setValueAsync") { value: String ->
-      // Send an event to JavaScript.
-      sendEvent("onChange", mapOf(
-        "value" to value
-      ))
-    }
-
-    // Enables the module to be used as a native view. Definition components that are accepted as part of
-    // the view definition: Prop, Events.
     View(ConicGradientView::class) {
-      // Defines a setter for the `url` prop.
-      Prop("url") { view: ConicGradientView, url: URL ->
-        view.webView.loadUrl(url.toString())
+      Prop("colors") { view: ConicGradientView, colors: IntArray ->
+        view.setColors(colors)
       }
-      // Defines an event that the view can send to JavaScript.
-      Events("onLoad")
+
+      Prop("center") { view: ConicGradientView, center: FloatArray ->
+        view.setCenter(center)
+      }
+
+      Prop("angle") { view: ConicGradientView, angle: Float ->
+        view.setAngle(angle)
+      }
+
+      OnViewDidUpdateProps { view: ConicGradientView ->
+        view.invalidate()
+      }
     }
   }
 }
--- android/src/main/java/expo/modules/conicgradient/ConicGradientView.kt
+++ android/src/main/java/expo/modules/conicgradient/ConicGradientView.kt
@@ -1,30 +1,53 @@
 package expo.modules.conicgradient
 
 import android.content.Context
-import android.webkit.WebView
-import android.webkit.WebViewClient
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.SweepGradient
+import android.util.Log
+import androidx.core.graphics.transform
 import expo.modules.kotlin.AppContext
-import expo.modules.kotlin.viewevent.EventDispatcher
 import expo.modules.kotlin.views.ExpoView
 
 class ConicGradientView(context: Context, appContext: AppContext) : ExpoView(context, appContext) {
-  // Creates and initializes an event dispatcher for the `onLoad` event.
-  // The name of the event is inferred from the value and needs to match the event name defined in the module.
-  private val onLoad by EventDispatcher()
+  private var mCenter = floatArrayOf(0.5f, 0.5f)
+  private var mColors: IntArray = intArrayOf(Color.TRANSPARENT, Color.TRANSPARENT)
+  private var mAngle: Float = 0f
+  private var paint = Paint(Paint.ANTI_ALIAS_FLAG)
 
-  // Defines a WebView that will be used as the root subview.
-  internal val webView = WebView(context).apply {
-    layoutParams = LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT)
-    webViewClient = object : WebViewClient() {
-      override fun onPageFinished(view: WebView, url: String) {
-        // Sends an event to JavaScript. Triggers a callback defined on the view component in JavaScript.
-        onLoad(mapOf("url" to url))
-      }
-    }
+  fun setColors(colors: IntArray) {
+    mColors = colors
   }
 
-  init {
-    // Adds the WebView to the view hierarchy.
-    addView(webView)
+  fun setCenter(center: FloatArray) {
+    mCenter = center
+  }
+
+  fun setAngle(angle: Float) {
+    mAngle = angle
+  }
+
+  private fun updateGradient() {
+    val sweepGradient = SweepGradient(
+      width * mCenter[0],
+      height * mCenter[1],
+      mColors,
+      null
+    )
+    sweepGradient.transform {
+      setRotate(
+        mAngle - 90f,
+        width * mCenter[0],
+        height * mCenter[1]
+      )
+    }
+    paint.shader = sweepGradient
+  }
+
+  override fun dispatchDraw(canvas: Canvas) {
+    super.dispatchDraw(canvas)
+    updateGradient()
+    canvas.drawRect(0f, 0f, width.toFloat(), height.toFloat(), paint)
   }
 }
