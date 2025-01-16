b67c5e889ee97c758dfb2b64146bafd28198ea02
Jakub Grzywacz <kontakt@jakubgrzywacz.pl>
2025-01-16 22:58:53 +0100

    docs: update examples

--- example/App.tsx
+++ example/App.tsx
@@ -5,12 +5,40 @@ export default function App() {
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView style={styles.container}>
-        <Text style={styles.header}>Module API Example</Text>
-        <Group name="Views">
+        <Text style={styles.header}>Conic gradient</Text>
+        {/* MDN Examples: https://developer.mozilla.org/en-US/docs/Web/CSS/gradient/conic-gradient */}
+        {/* Some properties are not available yet */}
+        {/* background: conic-gradient(red, orange, yellow, green, blue); */}
+        <Group name="Example #1">
           <ConicGradientView
-            colors={["#ff0000", "#00ff00", "#0000ff", "#ff0000"]}
-            center={[0.2, 0.2]}
+            colors={["red", "orange", "yellow", "green", "blue"]}
+            style={styles.view}
+          />
+        </Group>
+        {/* background: conic-gradient(from 0.25turn at 50% 30%, #f69d3c, 10deg, #3f87a6, 350deg, #ebf8e1); */}
+        <Group name="Example #2">
+          <ConicGradientView
+            colors={["#f69d3c", "#3f87a6", "#ebf8e1"]}
+            center={[0.6, 0.3]}
+            angle={90}
+            style={styles.view}
+          />
+        </Group>
+        {/* background: conic-gradient(from 3.1416rad at 10% 50%, #e66465, #9198e5); */}
+        <Group name="Example #3">
+          <ConicGradientView
+            colors={["#e66465", "#9198e5"]}
+            center={[0.1, 0.5]}
             angle={180}
+            style={styles.view}
+          />
+        </Group>
+        {/* background: conic-gradient( 
+              red 6deg, orange 6deg 18deg, yellow 18deg 45deg, 
+              green 45deg 110deg, blue 110deg 200deg, purple 200deg); */}
+        <Group name="Example #4">
+          <ConicGradientView
+            colors={["red", "orange", "yellow", "green", "blue", "purple"]}
             style={styles.view}
           />
         </Group>
@@ -49,6 +77,6 @@ const styles = {
   },
   view: {
     flex: 1,
-    height: 300,
+    height: 200,
   },
 };
