df3b1b1e8cf1bdf1e7fa6d3c3a0fb324c8ec8497
Jakub Grzywacz <kontakt@jakubgrzywacz.pl>
2025-01-19 19:26:18 +0100

    feat: implement locations on iOS

--- ios/ConicGradientProps.swift
+++ ios/ConicGradientProps.swift
@@ -3,8 +3,7 @@ import ExpoModulesCore
 
 class ConicGradientProps: ExpoSwiftUI.ViewProps {
   @Field var colors: [Color] = []
+  @Field var locations: [Double] = []
   @Field var center: UnitPoint = .center
   @Field var angle: Double = 0
-//  @Field var startAngle: Double = 0
-//  @Field var endAngle: Double = 360
 }
--- ios/ConicGradientView.swift
+++ ios/ConicGradientView.swift
@@ -7,12 +7,18 @@ struct ConicGradientView: ExpoSwiftUI.View {
     var body: some View {
         ZStack(alignment: .topLeading) {
             AnyView(
-                AngularGradient(
-                    colors: props.colors,
-                    center: props.center,
-                    angle: Angle(degrees: props.angle - 90)
-                )
+                AngularGradient(stops: createStops(), center: props.center, angle: Angle(degrees: props.angle - 90))
             )
+        }
+    }
+    
+    private func createStops() -> [Gradient.Stop] {
+        var locations = props.locations
+        if props.colors.count != props.locations.count {
+            locations = Array(stride(from: 0.0, through: 1.0, by: 1.0 / Double(props.colors.count - 1) ))
+        }
+        return zip(props.colors, locations).map { (color, location) in
+            return Gradient.Stop(color: color, location: location)
         }
     }
 }
